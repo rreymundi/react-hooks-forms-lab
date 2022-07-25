0e5df50267ab69a2d82d9e44784c9142656b5bab
Raúl Reymundi <raul@kickstarter.com>
2022-07-25 18:09:02 -0400

    commit tree b6ccf97839ef6c0b63d23a96497d3f7c7a33646d

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
-  return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-
-      <label>
-        Category:
-        <select name="category">
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
-
-      <button type="submit">Add to List</button>
-    </form>
-  );
-}
-
-export default ItemForm;
+function ItemForm({ onItemFormSubmit }) {
+    const [formData, setFormData] = useState({
+      name: "",
+      category: "Produce",
+    });
+  
+    function handleChange(event) {
+      setFormData({
+        ...formData,
+        [event.target.name]: event.target.value,
+      });
+    }
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+      onItemFormSubmit({
+        id: uuid(),
+        ...formData
+      });
+    }
+  
+    return (
+      <form className="NewItem" onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+  
+        <label>
+          Category:
+          <select
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+          >
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+          </select>
+        </label>
+  
+        <button type="submit">Add to List</button>
+      </form>
+    );
+  }
+  
+  export default ItemForm;
